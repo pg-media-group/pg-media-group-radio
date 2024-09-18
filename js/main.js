b324e06b1f1286fd7c17e51c027b29cc17519c46
gordonpeyton83 <122050732+gordonpeyton83@users.noreply.github.com>
2024-09-18 09:42:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -916,3 +916,12 @@
     initApp();
 
 })();
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
